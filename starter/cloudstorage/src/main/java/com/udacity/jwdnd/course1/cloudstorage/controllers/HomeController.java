0e289b82bdd5ba5dbfd0c49d20370ab1e5b44493
Patrick Gendotti <patrick.gendotti@gmail.com>
2022-03-18 14:25:28 -0700

    feat: Redirect remains on the same tab after user action - File Tab not automatically selected after changes to notes or credentials

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Credentials;
 import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
@@ -33,7 +35,7 @@ public class HomeController {
     }
 	
 	@GetMapping("/home")
-    public String homeView(Principal principal, Model model) {
+    public String homeView(Principal principal, Model model, @RequestParam(required = false) String currTab) {
 		User currUser = userService.getUser(principal.getName());
 		System.out.println("The user returned for home is: " + currUser);
 		
@@ -41,33 +43,40 @@ public class HomeController {
 		System.out.println("Number of notes: " + currUserNotes.size());
 		if(currUserNotes.size() > 0) System.out.println(currUserNotes.get(0).getTitle());
 		model.addAttribute("userNotes", currUserNotes);
+		if(currTab == null) {
+			currTab = "fileTab";
+		}
+		model.addAttribute("currTab", currTab);
         return "home";
     }
 	
 	@PostMapping("/addNote")
-    public String addNote(Principal principal, Notes note, Model model) {
+    public String addNote(Principal principal, Notes note, Model model, RedirectAttributes redirectAttrs) {
 		System.out.println(principal.getName());
 		User currUser = userService.getUser(principal.getName());
 		System.out.println("The user returned is: " + currUser);
 		noteService.addNote(currUser.getUserId(), note);
+		redirectAttrs.addAttribute("currTab", "noteTab");
         return "redirect:/home";
     }
 	
 	@GetMapping("/deleteNote/{noteId}")
-    public String deleteNote(Principal principal, Model model, @PathVariable int noteId) {
+    public String deleteNote(Principal principal, Model model, @PathVariable int noteId, RedirectAttributes redirectAttrs) {
 		System.out.println(principal.getName());
 		User currUser = userService.getUser(principal.getName());
 		System.out.println("The user returned is: " + currUser);
 		noteService.deleteNote(currUser.getUserId(), noteId);
+		redirectAttrs.addAttribute("currTab", "noteTab");
         return "redirect:/home";
     }
 	
 	@PostMapping("/addCredential")
-    public String addCredential(Principal principal, Credentials credential, Model model) {
+    public String addCredential(Principal principal, Credentials credential, Model model, RedirectAttributes redirectAttrs) {
 		System.out.println(principal.getName());
 		User currUser = userService.getUser(principal.getName());
 		System.out.println("The user returned is: " + currUser);
 		credentialService.addCredential(currUser.getUserId(), credential);
+		redirectAttrs.addAttribute("currTab", "credentialTab");
         return "redirect:/home";
     }
 }
