105214c81127d3ca277d80da0e19b44c345d6391
Patrick Gendotti <patrick.gendotti@gmail.com>
2022-04-02 10:02:38 -0700

    feat: Complete note add, edit, and remove test coverage

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -9,21 +10,55 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+
 import java.io.File;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CloudStorageApplicationTests {
 
 	@LocalServerPort
 	private int port;
 
 	private WebDriver driver;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private NoteService noteService;
+	
+	@Autowired
+	private User elmerFudd;
+	
+	@Autowired
+	private Notes existingNote;
 
 	@BeforeAll
-	static void beforeAll() {
+	void beforeAll() {
 		WebDriverManager.chromedriver().setup();
+		
+		// add a user here that serves as an existing user (so signups are not repeated for tests)
+		// directly add to DB - do not use signup form!
+		elmerFudd.setUsername("WabbitHunter007");
+		elmerFudd.setPassword("W4bbits");
+		elmerFudd.setFirstname("Elmer");
+		elmerFudd.setLastname("Fudd");
+		
+		int fuddId = userService.createUser(elmerFudd);
+		
+		// add an existing note for the existing user
+		existingNote.setTitle("Buy New Outfit");
+		existingNote.setDescription("I need to buy more hunting clothes to get the wabbits. Maybe camouflage will work better than dwab bwown!");
+		
+		int existingNoteId = noteService.addNote(fuddId, existingNote);
 	}
 
 	@BeforeEach
@@ -199,7 +234,140 @@ class CloudStorageApplicationTests {
 		Assertions.assertFalse(driver.getPageSource().contains("HTTP Status 403 – Forbidden"));
 
 	}
+	
+	// Test that the home page is not accessible without logging in
+	@Test
+	public void testHomePageAccess() {
+		driver.get("http://localhost:" + this.port + "/home");
+		
+		Assertions.assertFalse(driver.getTitle().contains("Home"));
+	}
 
+	// Test that the the user can sign up, login, and access the home page
+	// and then logout and no longer have access to the home page
+	@Test
+	public void testLogout() {
+		// Create a test account
+		doMockSignUp("URL","Test","UT2","123");
+		doLogIn("UT2", "123");
+		
+		// Check home page access
+		driver.get("http://localhost:" + this.port + "/home");
+		
+		Assertions.assertTrue(driver.getTitle().contains("Home"));
+		
+		// Logout
+		WebElement logoutButton = driver.findElement(By.id("logout"));
+		logoutButton.click();
+		
+		// Check home page access
+		driver.get("http://localhost:" + this.port + "/home");
+		
+		Assertions.assertFalse(driver.getTitle().contains("Home"));
+	}
+	
+	// Test that the the user can sign up, login, access the home page,
+	// and navigate to the note tab and add a new note
+	@Test
+	public void testAddNote() {
+		doLogIn("WabbitHunter007", "W4bbits");
+		
+		// add note
+		WebElement noteTab = driver.findElement(By.id("nav-notes-tab"));
+		noteTab.click();
+		
+		WebDriverWait webDriverWait = new WebDriverWait(driver, 2);
+		
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("addNote")));
+		WebElement addNoteButton = driver.findElement(By.id("addNote"));
+		addNoteButton.click();
+		
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("note-title")));
+		WebElement inputNoteTitle = driver.findElement(By.id("note-title"));
+		inputNoteTitle.click();
+		inputNoteTitle.sendKeys("Note Title Here");
+		
+		WebElement inputNoteDescription = driver.findElement(By.id("note-description"));
+		inputNoteDescription.click();
+		inputNoteDescription.sendKeys("Note Description Here");
+		
+		WebElement saveNoteButton = driver.findElement(By.id("postNote"));
+		saveNoteButton.click();
+		
+		WebElement addedNoteTitle = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div[1]/table/tbody/tr[3]/th"));
+		Assertions.assertEquals("Note Title Here", addedNoteTitle.getText());
+		
+		WebElement addedNoteDesc = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div[1]/table/tbody/tr[3]/td[2]"));
+		Assertions.assertEquals("Note Description Here", addedNoteDesc.getText());
+	}
+	
+	// Test that the the user can sign up, login, access the home page,
+	// and navigate to the note tab and edit an existing note
+	@Test
+	public void testEditNote() {
+		doLogIn("WabbitHunter007", "W4bbits");
+		
+		// edit note
+		WebElement noteTab = driver.findElement(By.id("nav-notes-tab"));
+		noteTab.click();
+		
+		WebDriverWait webDriverWait = new WebDriverWait(driver, 2);
+		
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("addNote")));
+		WebElement addNoteButton = driver.findElement(By.id("editNote1"));
+		addNoteButton.click();
+		
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("note-title")));
+		WebElement inputNoteTitle = driver.findElement(By.id("note-title"));
+		inputNoteTitle.click();
+		inputNoteTitle.clear();
+		inputNoteTitle.sendKeys("Edited Note Title");
+		
+		WebElement inputNoteDescription = driver.findElement(By.id("note-description"));
+		inputNoteDescription.click();
+		inputNoteDescription.clear();
+		inputNoteDescription.sendKeys("Edited Note Description");
+		
+		WebElement saveNoteButton = driver.findElement(By.id("postNote"));
+		saveNoteButton.click();
+		
+		WebElement firstNoteTitle = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div[1]/table/tbody/tr[2]/th"));
+		Assertions.assertEquals("Edited Note Title", firstNoteTitle.getText());
+		
+		WebElement firstNoteDesc = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div[1]/table/tbody/tr[2]/td[2]"));
+		Assertions.assertEquals("Edited Note Description", firstNoteDesc.getText());
+	}
+	
+	// Test that the the user can sign up, login, access the home page,
+	// and navigate to the note tab and delete an existing note
+	@Test
+	public void testDeleteNote() {
+		doLogIn("WabbitHunter007", "W4bbits");
+		
+		// delete note
+		WebElement noteTab = driver.findElement(By.id("nav-notes-tab"));
+		noteTab.click();
+		
+		WebDriverWait webDriverWait = new WebDriverWait(driver, 2);
+		
+		// Get the existing note's title and description - may be changed by testEditNote
+		// depending on the randomized test order, so not hard-coded here!
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("deleteNote1")));
+		
+		WebElement firstNoteTitle = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div[1]/table/tbody/tr[2]/th"));
+		String existingNoteTitle = firstNoteTitle.getText();
+		
+		WebElement firstNoteDesc = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div[1]/table/tbody/tr[2]/td[2]"));
+		String existingNoteDesc = firstNoteDesc.getText();
+		
+		// delete the existing note (first note)
+		WebElement deleteNoteButton = driver.findElement(By.id("deleteNote1"));
+		deleteNoteButton.click();
+		
+		// Check that the title and description are no longer on the page
+		webDriverWait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("deleteNote1")));
 
+		Assertions.assertFalse(driver.getPageSource().contains(existingNoteTitle) && driver.getPageSource().contains(existingNoteDesc));		
+	}
 
 }
